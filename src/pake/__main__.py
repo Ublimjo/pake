adea383a1d9dd47d94ea9bfee2966d87e69da868
Ublim <ublimjo@gmail.com>
2018-07-20 22:55:14 +0300

    Linting

--- src/pake/__main__.py
+++ src/pake/__main__.py
@@ -1,19 +1,28 @@
+'''
+Main entry point module
+'''
+
 import os
 import imp
 import click
 
 
 def check_file(path='./', _file='_pake_.py'):
-    wd = os.path.abspath(path)
-    wf = os.path.join(wd, _file)
-    if os.path.isfile(wf):
-        return wf
-    else:
-        return False
+    '''
+    check if _pake_.py file exist and valid
+    '''
+    working_directory = os.path.abspath(path)
+    working_file = os.path.join(working_directory, _file)
+    if os.path.isfile(working_file):
+        return working_file
+    return False
 
 
-def execute(wf, cmd):
-    _pake_ = imp.load_source('_pake_', wf)
+def execute(working_file, cmd):
+    '''
+    execute function in _pake_
+    '''
+    _pake_ = imp.load_source('_pake_', working_file)
     if cmd:
         for element in cmd:
             exec('_pake_.{}()'.format(element))
@@ -25,8 +34,12 @@ def execute(wf, cmd):
 @click.command()
 @click.argument('cmd', nargs=-1)
 def main(cmd):
-    wf = check_file()
-    if wf:
-        execute(wf, cmd)
+    '''
+    Tool which controls the generation of executables and other non-source
+    files of a program from the program's source files
+    '''
+    working_file = check_file()
+    if working_file:
+        execute(working_file, cmd)
     else:
         print('_pake_.py file not found')
